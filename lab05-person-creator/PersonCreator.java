86b68678a33447ca4466c84dd437a5ec485f5312
AFRAH RAHIM <afrahrahim@adelphi.internal>
2016-09-15 11:50:18 -0400

    finish w/ function contract

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -1,7 +1,8 @@
 
 /**
  * Write a description of class PersonCreator here.
- * 
+ * This class is used to get the user's name, age, sex, height, weight, Social Security Number, 
+ * Address, Phone Number, DOB
  * @author (Afrah Rahim) 
  * @version (9/13/2016)
  */
@@ -19,7 +20,6 @@ public class PersonCreator
    
    public PersonCreator (String First, String Last)
    { Name = First + "  " + Last;
-      System.out.println (" Enter your first and last name:" + Name);
        age = 0;
        sex = " ";
        height = 0;
@@ -28,70 +28,142 @@ public class PersonCreator
        address = " ";
        dateofbirth = " ";
     }
+     
     
-    public void getAge (int years) 
+ /**
+ * This method is used to find the age of the user
+ * Since this is void, there is no ouput
+ * This method tells the age of user in years
+ */ 
+public void getAge (int years) 
     { 
     age = years;
-    }
+    } 
+    
+ /**
+ * This method is used to find the sex of the user
+ * Since this is void, there is no ouput
+ */
     
     public void UserSex (String personSex)
     {
         sex = personSex;
     }
     
-    public void getHeight (int cm)
+/**
+* This method is used to find the height of the user
+* Since this is void, there is no ouput
+* This method gets the height of the user in centimeters
+*/
+public void getHeight (int cm)
     {
         height = cm;
     }
     
-    public void getWeight (int lbs)
+ /**
+ * This method is used to find the weight of the user
+ * Since this is void, there is no ouput
+ *This method gets the weight of the user in pounds
+ */
+public void getWeight (int lbs)
     {
         weight = lbs;
     }
      
-    public void getSocialSecurityNumber (String number)
+ /**
+ * This method is used to get the social security number of the user
+ */
+public void getSocialSecurityNumber (int block1, int block2, int block3)
     {
-        socialsecuritynumber = number;
+        
+         if (block1 < 100 || block1 > 999)
+         {
+             System.err.println (" That is invalid. ");
+             
+            } else if (block2 < 100 || block2 >999) 
+            { 
+                System.err.println (" That is invalid. ");
+                
+            } else if (block3 < 1000 || block3 >9999) 
+            { 
+                System.err.println (" That is invalid. ");
+                
+            }
+            else {
+                 socialsecuritynumber = block1 + " - " + block2 + " - " + block3;
+                }
+            
     }
     
-    public void getAddress (String local)
+     /**
+ * This method is used to find the address of the user
+ * Since this is void, there is no ouput
+ */
+public void getAddress (String local)
     {
        address = local;
     }
     
-    public void getDateOfBirth (String DOB)
+     /**
+ * This method is used to find the date of birth of the user
+ * Since this is void, there is no ouput
+ */
+public void getDateOfBirth (String DOB)
     { 
         dateofbirth = DOB;
+
     }
- 
-     public void GetPhoneNumer ( String block1, String block2,String block3)
-     {
-         PhoneNumber = block1 + " " + block2 + " " + block3;
-         if ( 100 <= block1 <= 999)
-           {
-               System.out.println ( "block1 is accepted");
-            } else
-            {
-                System.out.println (" block1 is not accepted");
-            }
-
-{ if ( 100 <= block2 <= 999)
-     System.out.println ( "block2 is accepted");
-    } else
-    {System.out.println (" block2 is not accepted");  
-    }
-}
-
-{ if ( 1000 <= block3 <= 9999)
-     System.out.println ( "block3 is accepted");
-    } else
-    {System.out.println (" block3 is not accepted");  
-    }
-}
-
-}
-  
- 
-
     
-   
+      /**
+ * This method is used to find the user's phone number
+ */
+public void GetPhoneNumer ( int areaCode, int numBegin, int numEnd)
+     {
+         if (areaCode < 100 || areaCode > 999)
+         {
+             System.err.println (" That is invalid. ");
+             
+            } else if (numBegin < 100 || numBegin >999) 
+            { 
+                System.err.println (" That is invalid. ");
+                
+            } else if (numEnd < 1000 || numEnd >9999) 
+            { 
+                System.err.println (" That is invalid. ");
+                
+            }
+            else {
+                 phonenumber= areaCode + " - " + numBegin + " - " + numEnd;
+                }
+            
+           
+        }
+        
+      /**
+ * This method is used to print all the user's given information
+ * Since this is void, there is no ouput
+ */    public void printForm()
+    {
+       
+        System.out.println("User Name: " + Name);
+        System.out.println ("     ");
+        System.out.println("User Age:" + age);
+        System.out.println ("     ");
+        System.out.println("User Sex:" + sex);
+        System.out.println ("     ");
+        System.out.println("User Height:" + height);
+        System.out.println ("     ");
+        System.out.println("User Weight:" + weight);
+        System.out.println ("     ");
+        System.out.println("Address:" + address);
+        System.out.println ("     ");
+        System.out.println (" Date of Birth:" + dateofbirth);
+        System.out.println ("     ");
+        System.out.println("Phone Number:" + phonenumber);
+        System.out.println ("     ");
+        System.out.println("Social Security Number:" + socialsecuritynumber);
+
+        
+    }
+    } 
+    
